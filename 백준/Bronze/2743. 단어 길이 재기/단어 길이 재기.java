a3a3caf32f17aca63d9a9d93bf4f4bb25eb26c24
Learnercotae <137987513+Learnercotae@users.noreply.github.com>
2024-04-13 13:14:43 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 196 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		
+		String a=scanner.next();
+		
+		System.out.print(a.length());
+	}
+
+}
