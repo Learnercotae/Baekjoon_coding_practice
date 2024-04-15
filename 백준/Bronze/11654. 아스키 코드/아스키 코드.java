f2e19b353d006270010183bac72a74143ff74933
Learnercotae <137987513+Learnercotae@users.noreply.github.com>
2024-04-15 22:15:35 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17608 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		
+		int n=scanner.next().charAt(0); //String에서 char로, 문자열을 잘라 문자로 변환
+		//int 타입 변수에 저장함으로써 문자에 대응되는 값, 즉 아스키 코드값을 저장한다.
+		
+		System.out.print(n);
+		
+		
+	}
+
+}
