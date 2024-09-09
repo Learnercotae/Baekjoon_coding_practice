135dcbd47b482bfdc0b9414d9da20d1dc69dbd50
Learnercotae <137987513+Learnercotae@users.noreply.github.com>
2024-09-09 12:45:32 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,30 +1,14 @@
-# 정수 n이 주어졌을 때, n을 1,2,3의 합으로 나타내는 방법의 수를 구해라
+def func(x):
+  if x==1:
+    return 1
+  elif x==2:
+    return 2
+  elif x==3:
+    return 4
+  else:
+    return func(x-1)+func(x-2)+func(x-3)
 
-# 1. DP 배열 정의
-# 2. 기본 초기화
-# 3. 점화식 설정
-# 4. 반복문을 통한 계산
-
-T=int(input())
-
-def count_ways(n):
-    # dp 배열 초기화 (n+1 크기)
-    dp = [0] * (n + 1)
-    
-    # 초기 조건 설정
-    dp[0] = 1  # 0을 만드는 방법은 1가지 (아무 것도 선택하지 않는 것)
-    
-    # 점화식에 따라 dp 배열 채우기
-    for i in range(1, n + 1):
-        if i >= 1:
-            dp[i] += dp[i - 1]
-        if i >= 2:
-            dp[i] += dp[i - 2]
-        if i >= 3:
-            dp[i] += dp[i - 3]
-    
-    return dp[n]
-
-for i in range(0,T):
-    n = int(input())
-    print(count_ways(n))
+t = int(input())
+for _ in range(t):
+  n = int(input())
+  print(func(n))
