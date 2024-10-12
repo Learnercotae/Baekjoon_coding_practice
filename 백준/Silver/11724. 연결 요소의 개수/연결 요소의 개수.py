5f04be8b6ea30eef3b010163b0d63d45971c2183
Learnercotae <137987513+Learnercotae@users.noreply.github.com>
2024-10-12 17:39:25 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 580 ms, Memory: 65156 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,37 +1,33 @@
+# 방향 없는 그래프 = 양방향 그래프
 import sys
-sys.setrecursionlimit(2000)  # 재귀 깊이 제한을 늘려줍니다.
-input = sys.stdin.readline
+sys.setrecursionlimit(2000)  # 재귀 깊이 제한을 늘립니다.
+input=sys.stdin.readline
 
-# 정점의 개수 N과 간선의 개수 M 입력
-N, M = map(int, input().split())
+N,M=map(int,input().strip().split())
+graph=[[] for _ in range(N+1)]
 
-# 그래프를 인접 리스트로 표현
-graph = [[] for _ in range(N + 1)]
-
-# 간선 정보 입력
-for _ in range(M):
-    u, v = map(int, input().split())
+for i in range (M):
+    u,v=map(int,input().strip().split())
     graph[u].append(v)
     graph[v].append(u)
 
-# 방문 여부를 확인하기 위한 리스트
-visited = [False] * (N + 1)
 
-# DFS 함수 정의
+visited=[0]*(N+1)
+
 def dfs(x):
-    visited[x] = True
-    for neighbor in graph[x]:
-        if not visited[neighbor]:
-            dfs(neighbor)
+    visited[x]=1
+    for i in graph[x]:
+        if not visited[i]:
+            dfs(i)
 
-# 연결 요소 개수를 세기 위한 변수
-count = 0
+count=0
 
-# 모든 정점을 순회하면서 연결 요소 찾기
-for i in range(1, N + 1):
+for i in range(1,N+1):
     if not visited[i]:
         dfs(i)
-        count += 1
+        count+=1
 
-# 연결 요소의 개수 출력
+
 print(count)
+
+
