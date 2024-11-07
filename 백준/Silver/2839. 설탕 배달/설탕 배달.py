d3b2a7a2e14e6e95b6fd3dea40da5e0871cee2eb
Learnercotae <137987513+Learnercotae@users.noreply.github.com>
2024-11-07 10:46:10 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,28 +1,15 @@
-# 3킬로 봉지와 5킬로 봉지가 있음.
-
-# 첫째 줄: N
-# N을 입력받고, 먼저 N을 5로 나눈 뒤, 나머지를 3으로 나눔.
+import sys
+input=sys.stdin.readline
 
 n=int(input())
 
-
-# ex) 6일때는, 먼저 5로 나누어서 안되니까, 3으로 넘어가서 2를 출력해야함.
-
-
-if n%5 ==0: # 5로 나누어 떨어질 때
-    print(n//5)
+p=0
+while n>=0:
+    if n%5==0: # 5의 배수이면
+        p+=(n//5) # 5로 나눈 몫을 구해야 정수가 됨
+        print(p)
+        break
+    n-=3
+    p+=1 # 5의 배수가 될 때까지 설탕 -3, 봉지+1
 else:
-    p=0
-    while n>0:
-        n-=3
-        p+=1
-        if n%5==0: # 3Kg과 5Kg을 조합해서 담을 수 있을 때
-            p+=n//5
-            print(p)
-            break
-        elif n==1 or n==2: # 설탕 봉지만으로 나눌 수 없을 때
-            print(-1)
-            break
-        elif n==0:
-            print(p) # 3으로 나누어 떨어질 때
-            break
+    print(-1)
